44a4c6a08db02f07b6456ef326762485393961da
Adam Burdett <burdettadam@gmail.com>
2019-04-24 18:41:52 -0600

    feat(tests): added test suite
    Signed-off-by: burdettadam <burdettadam@gmail.com>

---
+++ test/Poc.Mobile.App.Tests/AgentContextTests.cs
@@ -0,0 +1,30 @@
+﻿using Poc.Mobile.App.Services;
+using System.Reflection;
+using Xunit;
+
+namespace Poc.Mobile.App.Tests
+{
+    public class AgentContextTests
+    {
+        private AgentContext context = new AgentContext
+            {
+                Did = "PNQm3CwyXbN5e39Rw3dXYx",
+                Verkey = "~AHtGeRXtGjVfXALtXP9WiX"
+                //Connection = "",
+                //Wallet = "",
+            };
+
+        [Fact]
+        public void AgentContextHasProperties()
+        {
+            Assert.True( context.GetType().GetTypeInfo().GetRuntimeProperty("Pool") != null );
+            Assert.True( context.GetType().GetTypeInfo().GetRuntimeProperty("State") != null );
+            Assert.True( context.GetType().GetTypeInfo().GetRuntimeProperty("Connection") != null );
+            Assert.True( context.GetType().GetTypeInfo().GetRuntimeProperty("Wallet") != null );
+            Assert.True( context.GetType().GetTypeInfo().GetRuntimeProperty("Did") != null );
+            Assert.True( context.GetType().GetTypeInfo().GetRuntimeProperty("Verkey") != null );
+        }
+
+
+    }
+}
