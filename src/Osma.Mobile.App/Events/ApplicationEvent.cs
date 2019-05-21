f477b84feeb9b22c96a28408cc1a334bebec3fd5
Tobias Looker <tplooker@gmail.com>
2019-05-21 16:34:11 +1200

    feat(connections): update connections functionality

---
+++ src/Osma.Mobile.App/Events/ApplicationEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Osma.Mobile.App.Events
+{
+    public enum ApplicationEventType
+    {
+        ConnectionsUpdated
+    }
+
+    public class ApplicationEvent
+    {
+        public ApplicationEventType Type { get; set; }
+    }
+}
