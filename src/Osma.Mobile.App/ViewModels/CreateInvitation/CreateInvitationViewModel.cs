be9d8271ba91a9385bca2593b84e344fa16c325a
Sukalpo Mitra <sukalpomitra@gmail.com>
2019-08-01 03:14:25 +0800

    feat(invite): add the ability to create an invitation (#31)

---
+++ src/Osma.Mobile.App/ViewModels/CreateInvitation/CreateInvitationViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Acr.UserDialogs;
+using AgentFramework.Core.Contracts;
+using Osma.Mobile.App.Services.Interfaces;
+using ReactiveUI;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+using AgentFramework.Core.Extensions;
+
+namespace Osma.Mobile.App.ViewModels.CreateInvitation
+{
+    public class CreateInvitationViewModel : ABaseViewModel
+    {
+        private readonly ICustomAgentContextProvider _agentContextProvider;
+        private readonly IConnectionService _connectionService;
+
+        public CreateInvitationViewModel(
+            IUserDialogs userDialogs,
+            INavigationService navigationService,
+            ICustomAgentContextProvider agentContextProvider,
+            IConnectionService defaultConnectionService
+            ) : base(
+                "CreateInvitation",
+                userDialogs,
+                navigationService
+           )
+        {
+            _agentContextProvider = agentContextProvider;
+            _connectionService = defaultConnectionService;
+        }
+
+        public override async Task InitializeAsync(object navigationData)
+        {
+            await base.InitializeAsync(navigationData);
+        }
+
+        private async Task CreateInvitation()
+        {
+            try
+            {
+                var context = await _agentContextProvider.GetContextAsync();
+                var (invitation, _) = await _connectionService.CreateInvitationAsync(context);
+
+                string barcodeValue = invitation.ServiceEndpoint + "?c_i=" + MessageUtils(invitation.ToJson().ToBase64());
+                QrCodeValue = barcodeValue;
+            }
+            catch (Exception ex)
+            {
+                DialogService.Alert(ex.Message);
+            }
+        }
+
+        private ZXingBarcodeImageView QRCodeGenerator(String barcodeValue)
+        {
+            var barcode = new ZXingBarcodeImageView
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                AutomationId = "zxingBarcodeImageView",
+            };
+
+            barcode.BarcodeFormat = ZXing.BarcodeFormat.QR_CODE;
+            barcode.BarcodeOptions.Width = 300;
+            barcode.BarcodeOptions.Height = 300;
+            barcode.BarcodeOptions.Margin = 10;
+            barcode.BarcodeValue = barcodeValue;
+
+            return barcode;
+
+        }
+
+        #region Bindable Command
+
+        public ICommand CreateInvitationCommand => new Command(async () => await CreateInvitation());
+
+        #endregion
+
+        #region Bindable Properties
+
+        private string _qrCodeValue;
+
+        public string QrCodeValue
+        {
+            get => _qrCodeValue;
+            set => this.RaiseAndSetIfChanged(ref _qrCodeValue, value);
+        }
+
+        #endregion
+    }
+}
