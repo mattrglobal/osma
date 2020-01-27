6b48b0af227c5e63e0f42f827ed17d4a664e665c
Sam Curren <telegramsam@gmail.com>
2020-01-27 14:37:25 -0700

    Fix invitation encoding
    Signed-off-by: Sam Curren <telegramsam@gmail.com>

--- src/Osma.Mobile.App/ViewModels/CreateInvitation/CreateInvitationViewModel.cs
+++ src/Osma.Mobile.App/ViewModels/CreateInvitation/CreateInvitationViewModel.cs
@@ -8,6 +8,7 @@ using ReactiveUI;
 using Xamarin.Forms;
 using ZXing.Net.Mobile.Forms;
 using Hyperledger.Aries.Features.DidExchange;
+using Hyperledger.Aries.Extensions;
 
 namespace Osma.Mobile.App.ViewModels.CreateInvitation
 {
@@ -43,7 +44,7 @@ namespace Osma.Mobile.App.ViewModels.CreateInvitation
                 var context = await _agentContextProvider.GetContextAsync();
                 var (invitation, _) = await _connectionService.CreateInvitationAsync(context);
 
-                string barcodeValue = invitation.ServiceEndpoint + "?c_i=";// + invitation.ToJson().ToBase64();
+                string barcodeValue = invitation.ServiceEndpoint + "?c_i=" + invitation.ToJson().ToBase64();
                 QrCodeValue = barcodeValue;
             }
             catch (Exception ex)
